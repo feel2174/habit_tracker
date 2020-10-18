c2d884615109ddaa04d969e62e005976e574e539
feel2174 <feel2174@gmail.com>
2020-10-18 15:29:01 +0900

    Updates

---
+++ precache-manifest.24b5145bf81e4a1c4ee70717bddb3d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba4e2e5c5301f863a7aaeb85a2b96cf",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "633992e23262c58c29fc",
+    "url": "/habit_tracker/static/css/main.9b39fd97.chunk.css"
+  },
+  {
+    "revision": "a915e9d2730f9a017c34",
+    "url": "/habit_tracker/static/js/2.aa543aad.chunk.js"
+  },
+  {
+    "revision": "76aa3a6712dcb2b7248a77b49e0cd963",
+    "url": "/habit_tracker/static/js/2.aa543aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633992e23262c58c29fc",
+    "url": "/habit_tracker/static/js/main.88f68852.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
